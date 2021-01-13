e7b8af6b7589a0ca3f81ea8ec40128dbe1a19c9a
yosmiya <gigongben@gmail.com>
2021-01-13 21:55:47 +0900

    Updates

---
+++ precache-manifest.b49264cbcc6564f8b687c494d496032b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a961404e9b8468fa26c130c12990bb",
+    "url": "/cyberelders/index.html"
+  },
+  {
+    "revision": "133af5bd330bb95d0a3c",
+    "url": "/cyberelders/static/css/main.cd6a83e7.chunk.css"
+  },
+  {
+    "revision": "c239cda7be5a7db3bd3e",
+    "url": "/cyberelders/static/js/2.abacd08c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cyberelders/static/js/2.abacd08c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133af5bd330bb95d0a3c",
+    "url": "/cyberelders/static/js/main.234611c5.chunk.js"
+  },
+  {
+    "revision": "c109b77cbf59ad273490",
+    "url": "/cyberelders/static/js/runtime-main.9076c17c.js"
+  },
+  {
+    "revision": "5f5288a6dbda4fe3cadbb8c30e37baa9",
+    "url": "/cyberelders/static/media/hero.5f5288a6.jpg"
+  },
+  {
+    "revision": "ab7a91e5ff828d1f3cb1296c5b90cd02",
+    "url": "/cyberelders/static/media/img01.ab7a91e5.jpg"
+  },
+  {
+    "revision": "800da152656629f87e3cc03ac630c9b2",
+    "url": "/cyberelders/static/media/img02.800da152.jpg"
+  },
+  {
+    "revision": "2c8cc4a64e1339f56547b5831a0d0504",
+    "url": "/cyberelders/static/media/php_logo.2c8cc4a6.svg"
+  },
+  {
+    "revision": "7d9fccaffda0db8340fe3e803fee9c83",
+    "url": "/cyberelders/static/media/python_logo.7d9fccaf.svg"
+  },
+  {
+    "revision": "da81c9c97f3b02541c634b5e712eeaa1",
+    "url": "/cyberelders/static/media/swift_logo.da81c9c9.svg"
+  },
+  {
+    "revision": "2aaf97ef3c97cdd38d61c679745059ba",
+    "url": "/cyberelders/static/media/teacher.2aaf97ef.png"
+  },
+  {
+    "revision": "21d0033f2c61645db737ef9eef8f53fb",
+    "url": "/cyberelders/static/media/teacher_achievement.21d0033f.jpg"
+  }
+]);
